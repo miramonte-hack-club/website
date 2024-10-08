d0fcd18082ec9a04ed9dcfc42694677992f3d655
Saghetti0 <weeb.saghetti@gmail.com>
2024-10-08 15:33:45 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["public/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "hc-red": "#ec3750",
+        "hc-muted": "#8492a6",
+      }
+    },
+  },
+  plugins: [],
+}
